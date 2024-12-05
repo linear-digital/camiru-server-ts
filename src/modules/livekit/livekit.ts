11e90d280f7720b0fa610c4dca99c0678d1bf339
MD TAMIZ UDDIN <tamizrabbi@gmail.com>
2024-12-05 21:49:28 +0600

    delete 1 file and update 2 files

--- src/modules/livekit/livekit.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, Router } from 'express';
-import { AccessToken } from 'livekit-server-sdk';
-const router = Router()
-
-// Token generation endpoint
-// Token generation route
-
-export default router
